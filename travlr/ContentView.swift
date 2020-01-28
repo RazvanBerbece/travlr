377af43c77a95a776b9d89ef589dbce81cece30b
Razvan-Antonio Berbece <sc19rab@leeds.ac.uk>
2020-01-28 01:31:51 +0000

    Initial Commit

---
+++ travlr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  travlr
+//
+//  Created by Razvan-Antonio Berbece on 28/01/2020.
+//  Copyright © 2020 Razvan-Antonio Berbece. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
